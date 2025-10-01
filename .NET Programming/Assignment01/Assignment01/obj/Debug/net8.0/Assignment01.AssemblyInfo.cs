57a7f7d29d2f7be44f0562055288738f063b1e54
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-10-01 23:57:42 +0300

    Added Druid AI Certificate 2

--- .NET Programming/Assignment01/Assignment01/obj/Debug/net8.0/Assignment01.AssemblyInfo.cs
+++ .NET Programming/Assignment01/Assignment01/obj/Debug/net8.0/Assignment01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d5cf1d103de8930a1cca5b7330364426f800c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5fc632dbdb7a9ff452e7ddc372d165ae78061e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
